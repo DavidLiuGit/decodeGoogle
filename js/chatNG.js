c95cad34130fc1ed0f72df540b02add2d1abc33a
Unknown <dvdgege5@hotmail.com>
2017-03-17 17:33:45 -0400

    Update Angular; add update script

--- js/chatNG.js
+++ js/chatNG.js
@@ -1,6 +1,14 @@
 (function() {
-	var app = angular.module('store', [ ]);				// create the module
+	var app = angular.module('chatRoom', [ ]);			// create the module
 
-    
+    // controller syntax:
+	app.controller ('chatController', function() {
+		this.messages = defaultMsgs;
+	});
+
+    var defaultMsgs = [
+        {sender:0, msg:"Welcome to the Antisocial Social App!"},
+        {sender:0, msg:"You can talk to yourself, because you're lonely as hell."}
+    ];
 
 })();
