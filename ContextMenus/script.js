46b7adaea29fd283a9acad67db3c13e298be449d
Olivia Perryman <Olivia Perryman>
2017-03-17 21:28:58 -0400

    Chrome extension works with post req

--- ContextMenus/script.js
+++ ContextMenus/script.js
@@ -1,11 +1,32 @@
 function getword(info,tab) {
   console.log("Word " + info.selectionText + " was clicked.");
-  chrome.tabs.create({  
+  sendPost(info.selectionText)
+  /*chrome.tabs.create({  
     url: "http://www.google.com/search?q=" + info.selectionText,
-  });           
+  });  */         
 }
 chrome.contextMenus.create({
   title: "Search: %s", 
   contexts:["selection"], 
   onclick: getword,
 });
+
+sendPost = function(sel){
+	var url = "http://localhost/";
+	var method = "POST";
+	var postData = sel;
+
+	var request = new XMLHttpRequest();
+
+	request.onload = function () {
+		var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+	    var data = request.responseText; // Returned data, e.g., an HTML document.
+	}
+
+	request.open(method, url);
+
+	request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+	request.send(postData);
+
+	document.getElementById('debug').innerHTML = "kek" ;
+}
--- ext/post.js
+++ ext/post.js
@@ -1,24 +1,30 @@
-
-
-
-var sel = window.getSelection().toString();
-
-//sel = "In Montreal with Google";
-
-var url = "http://localhost/";
-var method = "POST";
-var postData = sel;
-
-var request = new XMLHttpRequest();
-
-request.onload = function () {
-	var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
-    var data = request.responseText; // Returned data, e.g., an HTML document.
+function getWord(info,tab){
+ console.log("Word " + info.selectionText + " was clicked.");
+  sendPost(info.selectionText)        
 }
 
-request.open(method, url);
+chrome.contextMenus.create({
+  title: "Antisocial: %s", 
+  contexts:["selection"], 
+  onclick: getword,
+});
 
-request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
-request.send(postData);
+sendPost = function(sel){
+	var url = "http://localhost/";
+	var method = "POST";
+	var postData = sel;
 
-document.getElementById('debug').innerHTML = "kek" ;
+	var request = new XMLHttpRequest();
+
+	request.onload = function () {
+		var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+	    var data = request.responseText; // Returned data, e.g., an HTML document.
+	}
+
+	request.open(method, url);
+
+	request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8");
+	request.send(postData);
+
+	document.getElementById('debug').innerHTML = "kek" ;
+}
